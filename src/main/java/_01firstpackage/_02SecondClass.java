fc9f47cb1f42cf81e9d47c13b73e672bd760faa6
BAKHRIDIN KHODZHAEV <bakhridin@BAKHRIDINs-MacBook-Pro.local>
2022-07-23 21:29:42 -0700

    Adding hello class2

--- src/main/java/_01firstpackage/_02SecondClass.java
+++ src/main/java/_01firstpackage/_02SecondClass.java
@@ -1,2 +1,7 @@
-package _01firstpackage;public class _02SecondClass {
+package _01firstpackage;
+
+public class _02SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Hello class 2");
+    }
 }
